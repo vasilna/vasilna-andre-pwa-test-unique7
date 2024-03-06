59767fcce9882a35ee0030c2c8a280337a9cf9b4
vasilna <vasilna@uni-sofia.bg>
2024-03-06 10:51:28 +0200

    remove default from post handler

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -4,7 +4,7 @@ import prisma from '../../../lib/prisma';
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
-export default async function handle(req, res) {
+export async function handle(req, res) {
   const { title, content } = req.body;
 
   const session = await getSession({ req });
